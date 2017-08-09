2f40d1f91b3fe89cefcc4b39e3ea8aa578b353aa
Pedro Nunes <pnunes515@gmail.com>
2017-08-09 23:04:46 +0100

    Further experimentation with SQLite and Protobuffers

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(6, 19);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(534, 222);
+            this.textBoxLog.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxLog);
+            this.groupBox1.Location = new System.Drawing.Point(12, 240);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(546, 247);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Log";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 499);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace h4x0r_server
 {
@@ -15,6 +16,40 @@ namespace h4x0r_server
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Log("Server initialising...");
+            string filename = @"C:\Users\Valman\Dropbox\Dev\h4x0r\database\database.sqlite3";
+            m_DatabaseConnection = new SQLiteConnection("Data Source=" + filename + ";Version=3;");
+            //string accountsSql = "select * from Accounts;";
+            try
+            {
+                m_DatabaseConnection.Open();
+                //DataSet ds = new DataSet();
+                //var da = new SQLiteDataAdapter(accountsSql, m_DatabaseConnection);
+                //da.Fill(ds);
+                //dataGridView1.DataSource = ds.Tables[0].DefaultView;
+                Log("Connection to database estabilished.");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private SQLiteConnection m_DatabaseConnection;
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            m_DatabaseConnection.Close();
+        }
+
+        private void Log(string text)
+        {
+            DateTime d = DateTime.Now;
+            textBoxLog.Text += "[" + d.ToShortDateString() + " " + d.ToShortTimeString() + "]: " + text + Environment.NewLine;
         }
     }
 }
