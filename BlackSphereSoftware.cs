4abd36811d28c57815b48b2a521052eaba9fd62c
Pedro Nunes <pnunes515@gmail.com>
2017-10-29 21:51:33 +0000

    WIP software

---
+++ BlackSphereSoftware.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h4x0r
+{
+    class BlackSphereSoftware
+    {
+        static public List<File> Instantiate()
+        {
+            List<File> files = new List<File>();
+
+
+
+            return files;
+        }
+    }
+}
---
+++ File.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h4x0r
+{
+    class File
+    {
+        public File()
+        {
+            FileType = Type.Generic;
+            Version = 1;
+        }
+
+        public enum Type
+        {
+            Generic,
+            Ice,
+            Icebreaker,
+            Utility
+        };
+
+        public enum SubType
+        {
+            // Generic
+            Generic,
+
+            // Ice
+            Firewall,
+            Service,
+            ConnectionSentry,
+            PasswordGate,
+            Proxy,
+
+            // Icebreaker
+
+            // Utility
+            NodeAnalyser
+        }
+
+        public Type FileType { get; set; }
+        public SubType FileSubType { get; set; }
+        public string Name { get; set; }
+        public int Version { get; set; }
+    }
+}
