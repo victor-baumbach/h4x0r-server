584e11c2394a45190ef5083388dbde91a416a069
Pedro Nunes <pnunes515@gmail.com>
2018-01-17 14:07:23 +0000

    Added connections between two sets
    During network generation, the ingress and egress sets were disjoint. A number of connections is now established between them.
    Also fixed an issue where the connections looked too linear.

--- h4x0r-server/Run/NetworkGenerator.cs
+++ h4x0r-server/Run/NetworkGenerator.cs
@@ -106,6 +106,11 @@ namespace h4x0r.Run
             egressNodesToProcess.Add(egressNode);
             while (ingressNodesToProcess.Count != 0 && egressNodesToProcess.Count != 0)
             {
+                // The lists of notes to process need to be shuffled so nodes aren't discovered
+                // in a biased direction.
+                ShuffleList(ref ingressNodesToProcess);
+                ShuffleList(ref egressNodesToProcess);
+
                 ProcessNode(ingressNodesToProcess, ingressSet);
                 ProcessNode(egressNodesToProcess, egressSet);
             }
@@ -123,7 +128,6 @@ namespace h4x0r.Run
             Network.Node node = setToProcess[0];
             setToProcess.RemoveAt(0);
 
-            // TODO: Ensure all nodes have at least one link established.
             Network.LinkDirections directions = GenerateLinkDirections(node);
 
             if ((directions & Network.LinkDirections.Left) != 0)
@@ -200,24 +204,29 @@ namespace h4x0r.Run
                 if ((directions & Network.LinkDirections.Up) != 0) directionsList.Add(Network.LinkDirections.Up);
                 if ((directions & Network.LinkDirections.Down) != 0) directionsList.Add(Network.LinkDirections.Down);
 
-                ShuffleDirections(ref directionsList);
+                ShuffleList(ref directionsList);
 
                 directions = Network.LinkDirections.None;
                 directions |= directionsList[0];
-                if (directionsList.Count > 1 && (m_RandomGenerator.Next() % 2) == 0) directions |= directionsList[1];
+                if (directionsList.Count > 1 /*&& (m_RandomGenerator.Next() % 2) == 0*/) directions |= directionsList[1];
+
+                if (directionsList.Count <= 1)
+                {
+                    int a = 0;
+                }
             }
 
             return directions;
         }
 
-        private void ShuffleDirections(ref List<Network.LinkDirections> list)
+        private void ShuffleList<T>(ref List<T> list)
         {
             int n = list.Count;
             while (n > 1)
             {
                 n--;
                 int k = m_RandomGenerator.Next(n + 1);
-                Network.LinkDirections value = list[k];
+                T value = list[k];
                 list[k] = list[n];
                 list[n] = value;
             }
@@ -225,7 +234,101 @@ namespace h4x0r.Run
 
         private void ConnectSets(List<Network.Node> ingressSet, List<Network.Node>egressSet)
         {
+            List<PotentialConnection> potentialConnections = new List<PotentialConnection>();
 
+            // Create a grid containing all the nodes in the egress set so we can quickly
+            // check nodes without having to perform a linear search.
+            Network.Node[,] egressGrid = new Network.Node[m_Settings.Size, m_Settings.Size];
+            foreach (Network.Node node in egressSet)
+            {
+                egressGrid[node.X, node.Y] = node;
+            }
+
+            foreach (Network.Node node in ingressSet)
+            {
+                if (node.X - 1 >= 0 && egressGrid[node.X - 1, node.Y] != null)
+                {
+                    potentialConnections.Add(new PotentialConnection(node, egressGrid[node.X - 1, node.Y]));
+                }
+
+                if (node.X + 1 < m_Settings.Size && egressGrid[node.X + 1, node.Y] != null)
+                {
+                    potentialConnections.Add(new PotentialConnection(node, egressGrid[node.X + 1, node.Y]));
+                }
+
+                if (node.Y - 1 >= 0 && egressGrid[node.X, node.Y - 1] != null)
+                {
+                    potentialConnections.Add(new PotentialConnection(node, egressGrid[node.X, node.Y - 1]));
+                }
+
+                if (node.Y + 1 < m_Settings.Size && egressGrid[node.X, node.Y + 1] != null)
+                {
+                    potentialConnections.Add(new PotentialConnection(node, egressGrid[node.X, node.Y + 1]));
+                }
+            }
+
+            ShuffleList(ref potentialConnections);
+            int connectionsToEstablish = Math.Min(m_RandomGenerator.Next(m_Settings.Size - 1) + 1, potentialConnections.Count);
+            for (int i = 0; i < connectionsToEstablish; ++i)
+            {
+                EstablishConnection(potentialConnections[i]);
+            }
+        }
+
+        private void EstablishConnection(PotentialConnection pc)
+        {
+            if (pc.NodeA.X < pc.NodeB.X)
+            {
+                pc.NodeA.LinkDirections |= Network.LinkDirections.Right;
+                pc.NodeB.LinkDirections |= Network.LinkDirections.Left;
+            }
+            else if (pc.NodeA.X > pc.NodeB.X)
+            {
+                pc.NodeA.LinkDirections |= Network.LinkDirections.Left;
+                pc.NodeB.LinkDirections |= Network.LinkDirections.Right;
+            }
+            else if (pc.NodeA.Y < pc.NodeB.Y)
+            {
+                pc.NodeA.LinkDirections |= Network.LinkDirections.Down;
+                pc.NodeB.LinkDirections |= Network.LinkDirections.Up;
+            }
+            else if (pc.NodeA.Y > pc.NodeB.Y)
+            {
+                pc.NodeA.LinkDirections |= Network.LinkDirections.Up;
+                pc.NodeB.LinkDirections |= Network.LinkDirections.Down;
+            }
+        }
+
+        private class PotentialConnection
+        {
+            public PotentialConnection(Network.Node nodeA, Network.Node nodeB)
+            {
+                NodeA = nodeA;
+                NodeB = nodeB;
+            }
+
+            public Network.Node NodeA { get; set; }
+            public Network.Node NodeB { get; set; }
+
+            public static bool operator ==(PotentialConnection pc1, PotentialConnection pc2)
+            {
+                return ((pc1.NodeA == pc2.NodeA || pc1.NodeA == pc2.NodeB) && (pc1.NodeB == pc2.NodeA || pc1.NodeB == pc2.NodeB));
+            }
+
+            public static bool operator !=(PotentialConnection pc1, PotentialConnection pc2)
+            {
+                return !((pc1.NodeA == pc2.NodeA || pc1.NodeA == pc2.NodeB) && (pc1.NodeB == pc2.NodeA || pc1.NodeB == pc2.NodeB));
+            }
+
+            public override bool Equals(object obj)
+            {
+                return (this == (PotentialConnection)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return NodeA.GetHashCode() + NodeB.GetHashCode();
+            }
         }
 
         public Network.Node[,] Nodes { get; set; }
